913f81114048ceb781c5ae585a7ff63800da5b98
BadP <kasimovasophia@gmail.com>
2022-02-01 22:01:47 +0300

    changed execute Thread, move logic to different methods

--- src/com/homework/lesson10/Main.java
+++ src/com/homework/lesson10/Main.java
@@ -1,13 +1,13 @@
 package com.homework.lesson10;
 
-import java.io.IOException;
+import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.http.HttpClient;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.*;
+
 //Домашнее задание 10
 //
 //        .Многопоточность.
@@ -34,67 +34,46 @@ import java.util.concurrent.CopyOnWriteArrayList;
 //
 //        - Для получения содержимого используйте new URL().openStream() (не забывайте закрывать поток)
 public class Main {
-    static long startCount2=0;
-    public static void main(String[] args) throws InterruptedException {
-        List<String> https = new CopyOnWriteArrayList<>();
-        https.add("https://habr.com/");
-        https.add("https://github.com/");
-        https.add("https://www.google.com/");
-        long start = System.currentTimeMillis();
-        long startCount = 0;
-        for (String http : https) {
-            try (InputStream inputStream = new URL(http).openStream()) {
-                final byte[] bytes = inputStream.readAllBytes();
-                startCount += bytes.length;
-                System.out.println(startCount);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        List<String> httpsString =
+                Arrays.asList("https://habr.com/", "https://github.com/", "https://www.google.com/");
+        runSeries(httpsString);
 
+        runParallel(httpsString);
+    }
+
+    public static void runSeries(List<String> httpsString) {
+        List<Integer> lengths = new CopyOnWriteArrayList<>();
+        long start = System.currentTimeMillis();
+        for (String http : httpsString) {
+            lengths.add(getInfoFromLink(http));
         }
         long finish = System.currentTimeMillis();
         long elapsed = finish - start;
         System.out.println("Прошло времени, мс: " + elapsed);
-        System.out.println("Общий размер: " + startCount);
+        System.out.println("Общий размер: " + lengths.stream().reduce(Integer::sum));
+    }
 
-        long start2 = System.currentTimeMillis();
-        Thread thread1 = new Thread(() -> {
-            try (InputStream inputStream = new URL(https.get(0)).openStream()) {
-                final byte[] bytes = inputStream.readAllBytes();
-                startCount2 += bytes.length;
-                System.out.println(startCount2);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        Thread thread2 = new Thread(() -> {
-            try (InputStream inputStream = new URL(https.get(1)).openStream()) {
-                final byte[] bytes = inputStream.readAllBytes();
-                startCount2 += bytes.length;
-                System.out.println(startCount2);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-        Thread thread3 = new Thread(() -> {
-            try (InputStream inputStream = new URL(https.get(2)).openStream()) {
-                final byte[] bytes = inputStream.readAllBytes();
-                startCount2 += bytes.length;
-                System.out.println(startCount2);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+    public static void runParallel(List<String> httpsString) throws ExecutionException, InterruptedException {
+        List<Integer> lengths = new CopyOnWriteArrayList<>();
+        long start = System.currentTimeMillis();
+        ExecutorService executorService = Executors.newFixedThreadPool(httpsString.size());
+        for (String http : httpsString) {
+            lengths.add(executorService.submit(() -> getInfoFromLink(http)).get());
+        }
+        executorService.shutdown();
+        long finish = System.currentTimeMillis();
+        long elapsed = finish - start;
+        System.out.println("Прошло времени, мс: " + elapsed);
+        System.out.println("Общий размер: " + lengths.stream().reduce(Integer::sum));
+    }
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread1.join();
-        thread2.join();
-        thread3.join();
-        long finish2 = System.currentTimeMillis();
-        long elapsed2 = finish2 - start2;
-        System.out.println("Прошло времени, мс: " + elapsed2);
-        System.out.println("Общий размер: " + startCount2);
+    public static Integer getInfoFromLink(String http) {
+        try (BufferedInputStream inputStream = new BufferedInputStream(new URL(http).openStream())) {
+            return inputStream.readAllBytes().length;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 }
